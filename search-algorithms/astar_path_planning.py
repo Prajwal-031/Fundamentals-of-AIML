303121f516385a1d4da2f336da03002133bf88c3
Prajwal-031 <mpprajwal11@gmail.com>
2026-03-27 22:46:49 +0530

    Implement BFS pathfinding with obstacle handling in grid navigation

---
+++ search-algorithms/astar_path_planning.py
@@ -0,0 +1,56 @@
+import heapq
+
+def heuristic(x,y,gx,gy):
+    return abs(x-gx)+abs(y-gy)
+
+def astar(grid):
+
+    n=len(grid)
+    m=len(grid[0])
+
+    goal=(n-1,m-1)
+
+    pq=[]
+    heapq.heappush(pq,(0,0,0,[(0,0)]))
+
+    visited=set()
+
+    directions=[(-1,0),(1,0),(0,-1),(0,1)]
+
+    while pq:
+
+        f,x,y,path = heapq.heappop(pq)
+
+        if (x,y)==goal:
+            print("Final Path:",path)
+            print("Total Cost:",len(path)-1)
+            return
+
+        if (x,y) in visited:
+            continue
+
+        visited.add((x,y))
+
+        for dx,dy in directions:
+
+            nx=x+dx
+            ny=y+dy
+
+            if 0<=nx<n and 0<=ny<m and grid[nx][ny]==0:
+
+                g=len(path)
+                h=heuristic(nx,ny,goal[0],goal[1])
+
+                f=g+h
+
+                heapq.heappush(pq,(f,nx,ny,path+[(nx,ny)]))
+
+# Test the algorithm
+grid = [
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [1, 1, 0, 0, 0]
+]
+
+astar(grid)
